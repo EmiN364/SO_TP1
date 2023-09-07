f9420916b4dd54f401014cb07ba96e2f7b1944df
EmiN364 <68005487+EmiN364@users.noreply.github.com>
2023-09-07 17:30:44 -0300

    feat(app, slave): :poop: WIP Select in app, and send and receive files/md5 between app and slave
    Co-Authored-By: gcandisano <102882560+gcandisano@users.noreply.github.com>
    Co-Authored-By: Theo Shlamovitz <87035324+Theo-Shlamo@users.noreply.github.com>

--- src/app.c
+++ src/app.c
@@ -2,15 +2,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <sys/select.h>
+#include <fcntl.h>
 
 #define SLAVE_AMOUNT 5
 
-int stat(const char *restrict pathname, struct stat *restrict statbuf);
-pid_t fork(void);
-int execve(const char *pathname, char *const argv[], char *const envp[]);
-
 int main(int argc, char * argv[], char * envp[]) {
     printf("Cant args: %d\n", argc);
+    setvbuf(stdout, NULL, _IONBF, 0);
 
     char * files[argc - 1];
     int countFiles = 0;
@@ -27,13 +26,19 @@ int main(int argc, char * argv[], char * envp[]) {
     }
 
     printf("Cant Files: %d\n", countFiles);
-    for (int i = 0; i < countFiles; i++) {
+/*     for (int i = 0; i < countFiles; i++) {
         printf("File %d: %s\n", i, files[i]);
-    }
+    } */
 
     if (countFiles < 1) {
         puts("No files to process");
         return 0;
+    }
+
+    int outputFile = open("output.txt", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+    if (outputFile < 0) {
+        perror("Error while creating output file");
+        return 1;
     }
 
     int filesToSlaves = countFiles * 0.1;
@@ -46,18 +51,20 @@ int main(int argc, char * argv[], char * envp[]) {
     // TODO: Ver caso si es 0
 
     int filesSent = 0;
+    int filesProcessed = 0;
 
     int slavePids[SLAVE_AMOUNT];
 
     int fds[SLAVE_AMOUNT * 2];
 
     for (int i = 0 ; i < SLAVE_AMOUNT ; i++) {
-        char * slaveFiles[filesPerSlave + 1];
+        char * slaveFiles[filesPerSlave + 2];
+        slaveFiles[0] = "slave";
         if (filesSent < filesToSlaves) { // We check this in case filesPerSlave was 0
-            for (int j = 0; j < filesPerSlave; j++)
+            for (int j = 1; j < filesPerSlave + 1; j++)
                 slaveFiles[j] = files[filesSent++];
 
-            slaveFiles[filesPerSlave] = NULL;
+            slaveFiles[filesPerSlave + 1] = NULL;
         } else {
             slaveFiles[0] = NULL;
         }
@@ -84,19 +91,65 @@ int main(int argc, char * argv[], char * envp[]) {
 
             close(0); // stdin
             dup(writefd[0]); // read end of where app writes
-            close(writefd[0]);
             close(1); // stdout
             dup(readfd[1]); // write end of where app reads
+            close(writefd[0]);
             close(readfd[1]);
 
             execve("slave", slaveFiles, envp);
         } else {
             // Father
-
             close(writefd[0]);
             close(readfd[1]);
 
             slavePids[i] = pid;
         }
+    }
+
+    fd_set readfds;
+
+    while (filesProcessed < countFiles) {
+        printf("Files processed: %d\n", filesProcessed);
+
+        FD_ZERO(&readfds);
+        int nfds = 0;
+
+        for (int i = 0; i < SLAVE_AMOUNT; i++) {
+            FD_SET(fds[i*2], &readfds);
+
+            if (fds[i*2] >= nfds) nfds = fds[i*2] + 1;
+        }
+
+        if (select(nfds, &readfds, NULL, NULL, NULL) < 0) {
+            perror("Error while doing select in app");
+            return 1;
+        }
+
+        for (int i = 0; i < SLAVE_AMOUNT; i++) {
+            if (FD_ISSET(fds[i*2], &readfds)) {
+                // There is new hash to add to output
+                char buff[128];
+                int len = read(fds[i*2], buff, 128);
+                if (len < 0) {
+                    perror("Error while reading fd in app");
+                    return 1;
+                }
+
+                printf("Writing 1\n");
+                if (write(outputFile, buff, len) < 0) {
+                    perror("Error while writing 1 in app");
+                    return 1;
+                }
+                printf("Writing mid\n");
+                if ((filesProcessed + 1) < countFiles && write(fds[2*i + 1], files[filesSent], sizeof (char *)) < 0) {
+                    perror("Error while writing 2 in app");
+                    return 1;
+                }
+                filesSent++;
+                printf("Writing 2\n");
+            }
+        }
+
+        filesProcessed++;
     }
 }
--- src/slave.c
+++ src/slave.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+
+void processInitialFile(char * file, char buff[]);
 
 int main(int argc, char * argv[]) {
+
+    // Turning off print buffering
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
     // printf("ESCLAVO: Cant args: %d\n", argc);
     int myPid = getpid();
 
-    char rtas[argc][128];
+    char rtas[argc-1][128];
 
     int files = 0;
 
-    for (int i = 0; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         // printf("ESCLAVO: Arg %d: %s\n", i, argv[i]);
-        if (*argv[i] != NULL) {
-            processInitialFile(argv[i], rtas[i]);
+        if (*argv[i] != 0) {
+            processInitialFile(argv[i], rtas[i-1]);
             files++;
         }
     }
 
     for (int i = 0; i < files; i++) {
         printf("%d:\t%s", myPid, rtas[i]);
+    }
+
+    while (1) {
+        char fileName[128];
+        int r = read(0, fileName, 128);
+        fileName[r] = 0;
+        if (r < 0) {
+            perror("Error while reading");
+            exit(1);
+        }
+        if (r == 0) {
+            printf("Slave ended");
+            exit(0);
+        }
+        if (fileName != NULL) {
+            char rta[128];
+            dprintf(2, "Processing: %s\n", fileName);
+            processInitialFile(fileName, rta);
+            printf("%d:\t%s", myPid, rta);
+            // write(1, rta, 128);
+        }
     }
 }
 
@@ -28,7 +56,7 @@ void processInitialFile(char * file, char buff[]) {
 
     if (pipe(pipefds) != 0) {
         perror("Error while creating pipe in slave.");
-        return 1;
+        exit(1);
     }
 
     int pid = fork();
@@ -39,21 +67,27 @@ void processInitialFile(char * file, char buff[]) {
         // Child
         close(1); // Close stdout
         dup(pipefds[1]); // Write end of pipe
+        close(pipefds[0]);
+        close(pipefds[1]);
 
         char * args[3];
-        args[0] = file;
+        args[0] = "md5sum";
         args[1] = file;
         args[2] = NULL;
 
         execvp("md5sum", args);
+        perror("Errir while calling md5sum");
+        exit(1);        
     } else {
         int stat;
         waitpid(pid, &stat, 0);
         if (stat != 0) {
             perror("Error while doing md5 in slave");
-            return 1;
+            exit(1);
         }
         int r = read(pipefds[0], buff, 128);
         buff[r] = 0;
+        close(pipefds[0]);
+        close(pipefds[1]);
     }
 }
